d6f4e8e79e17ab5b7be228e4b198ce50f6ac7fa5
Shahid Khan <skhan@isystango.com>
2017-02-16 19:18:52 +0530

    shahid1

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class MyFirstReactNativeProject extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native! Hello 50555
+          Welcome to React Native! Hello 5055
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
